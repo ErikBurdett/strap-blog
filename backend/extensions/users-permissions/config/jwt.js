02bdb789b0e396d9e6803d0f05cc6336cc3fb91c
ErikBurdett <erikaburdet@gmail.com>
2021-07-19 11:57:28 -0500

    created app, 1 blog post, admin user, removed dummy data

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2db8e96d-9516-40f3-9d3a-a20ad7de99e4'
+};
---
+++ frontend/.cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\erika\\Desktop\\strapi-blog\\my-project\\frontend\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\erika\\Desktop\\strapi-blog\\my-project\\frontend\\src\\pages\\404.js")),
+  "component---src-pages-article-strapi-article-slug-js": preferDefault(require("C:\\Users\\erika\\Desktop\\strapi-blog\\my-project\\frontend\\src\\pages\\article\\{StrapiArticle.slug}.js")),
+  "component---src-pages-category-strapi-category-slug-js": preferDefault(require("C:\\Users\\erika\\Desktop\\strapi-blog\\my-project\\frontend\\src\\pages\\category\\{StrapiCategory.slug}.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\erika\\Desktop\\strapi-blog\\my-project\\frontend\\src\\pages\\index.js"))
+}
+
